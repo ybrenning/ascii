c5a31542e121eb081a4143ad5f5258d297335ce0
ybrenning <yannickbrenning2@gmail.com>
2024-03-25 15:34:18 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+var displayImage = document.getElementById("uploaded-img");
+var inputFile = document.getElementById("current-input");
+
+inputFile.onchange = function() {
+	displayImage.src = URL.createObjectURL(inputFile.files[0]);
+	console.log(inputFile.files[0]);
+
+	var canvas = document.createElement('canvas');
+	var context = canvas.getContext('2d');
+
+	displayImage.onload = function() { // Wait for the image to load
+		canvas.width = displayImage.width; // Set canvas dimensions to image dimensions
+		canvas.height = displayImage.height;
+		context.drawImage(displayImage, 0, 0); // Draw the image onto the canvas
+
+		var theData = context.getImageData(0, 0, displayImage.width, displayImage.height); // Get the image data
+		var pixels = theData.data
+		var grayPixels = [];
+		// console.log(pixels);
+		var sums = [];
+		
+		for (let i = 0; i < pixels.length; i+= 4) {
+			let sum = pixels[i] + pixels[i + 1] + pixels[i + 2];
+			sums.push(sum);
+			if (sum == 0) {
+				grayPixels.push("#");
+			} else if (0 < sum && sum <= 100) {
+				grayPixels.push("X");
+			} else if (100 < sum && sum <= 200) {
+				grayPixels.push("%");
+			} else if (200 < sum && sum <= 300) {
+				grayPixels.push("&");
+			} else if (300 < sum && sum <= 400) {
+				grayPixels.push("*");
+			} else if (400 < sum && sum <= 500) {
+				grayPixels.push("+");
+			} else if (500 < sum && sum <= 600) {
+				grayPixels.push("/");
+			} else if (600 < sum && sum <= 700) {
+				grayPixels.push(")");
+			} else {
+				grayPixels.push("'")
+			}
+		}
+		let text = "";
+
+		// console.log("sums");
+		// console.log(sums);
+		for (let y = 0; y < displayImage.height; y++) {
+			for (let x = 0; x < displayImage.width; x++) {
+				text = text + grayPixels[y*displayImage.width+x];
+			}
+			text = text + "\n";
+		}
+
+		console.log(text);
+		console.log(text.length);
+		document.getElementById("list").innerHTML = text;
+    };
+};
